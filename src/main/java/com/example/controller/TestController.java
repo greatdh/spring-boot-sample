9675bd4f76e014dd4b8b2293357daa92413d695c
Daehan Wi <greatdh12@gmail.com>
2016-05-01 19:20:05 +0900

    Spring fox & swagger 추가

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/test")
 public class TestController {
 
-    @RequestMapping(value = "/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> test() {
-        return new ResponseEntity<>("Hello World", HttpStatus.ACCEPTED);
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
     }
 }
